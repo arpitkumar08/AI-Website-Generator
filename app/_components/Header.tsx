09d7ccd031f5aea764baf624613617bcfd34e56b
Arpit Kumar <arpitk.9938@gmail.com>
2025-12-13 00:20:38 +0530

    initialized project

---
+++ app/_components/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+        {/* Logo */}
+        {/* Menu Options */}
+        {/* Get Started */}
+    </div>
+  )
+}
+
+export default Header
